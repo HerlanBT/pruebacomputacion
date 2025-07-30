95a1b61016077cb256ad4de25f01caf9161a58c1
HerlanBT <herlanballon777turpo@gmail.com>
2025-07-30 17:27:23 -0400

    6to

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d991309cf3614272ce2cf1b7c8cc903f
 "favicon.ico": "c0cb9cf0c9ce1345e0b4d4214c404554",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "f7efaa7831a5ab166b02d3c912f120d8",
+"flutter_bootstrap.js": "8c1bb3747d196ec24ccb5474dce2b192",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
